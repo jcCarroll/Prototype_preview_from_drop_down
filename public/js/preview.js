5090eaf1c10e246e8617676731353b9670ddfc59
2staffi@gmail.com <2staffi@gmail.com>
2017-07-24 11:26:23 -0400

    Finished population of preview, css, and dry code

--- public/js/preview.js
+++ public/js/preview.js
@@ -1,39 +1,10 @@
-function getArea() {
-    area = document.getElementById('area').value;
-    previewArea = document.getElementById('preview')
-    console.log(area);
-    areaText = document.createText(area);
-    previewArea.appendChild(area);
+function getInput(level) {
+    console.log(level);
+    selectedItem = document.getElementById(level).value;
+    previewArea = document.getElementById('preview');
+    pTag = document.createElement('p');
+    pTag.setAttribute('id', level + 'Selection');
+    selectedItemText = document.createTextNode(selectedItem);
+    pTag.appendChild(selectedItemText);
+    previewArea.appendChild(pTag);
 }
-
-function getList() {
-    list = document.getElementById('list').value;
-    console.log(list);
-}
-
-function getItem() {
-    item = document.getElementById('item').value;
-    console.log(item);
-}
-
-function getThing() {
-    thing = document.getElementById('thing').value;
-    console.log(thing);
-}
-
-// function exposureAreaSelection() {
-//     var exp = document.getElementById("area");
-//     // var label = document.createElement('label');
-//     var expInput = document.createElement("input");
-//     var br = document.createElement('br');
-//     label.innerHTML = "Exposure Area: ";
-//     expInput.setAttribute("type", "text");
-//     expInput.setAttribute('id', 'expArea');
-//     expInput.setAttribute('name', 'expArea');
-//     label.setAttribute("class", "expForm");
-//     // exp.appendChild(label);
-//     exp.appendChild(br);
-//     exp.appendChild(expInput);
-//     exp.appendChild(br);
-//     return false;
-// }
