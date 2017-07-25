63063a651de79ab23bff18d01c2338a8a7f9019c
2staffi@gmail.com <2staffi@gmail.com>
2017-07-25 11:42:27 -0400

    function to count and build id of element being created

--- public/js/preview.js
+++ public/js/preview.js
@@ -1,3 +1,13 @@
+var inputArea = 0;
+var inputList = 0;
+var inputItem = 0;
+var inputThing = 0;
+
+function createExp(level, location) {
+    expCounter(location);
+    getInput(level);
+}
+
 function getInput(level) {
     selectedItem = document.getElementById(level).value;
     previewArea = document.getElementById('preview');
@@ -5,7 +15,7 @@ function getInput(level) {
     aTag.setAttribute('href', '#');
     aTag.setAttribute('value', selectedItem)
     aTag.setAttribute('onclick', 'test()');
-    aTag.setAttribute('id', '1.0.0.0');
+    aTag.setAttribute('id', inputArea + "." + inputList + "." + inputItem + "." + inputThing);
     aTag.setAttribute('class', level + 'Selection')
     selectedItemText = document.createTextNode(selectedItem);
     aTag.appendChild(selectedItemText);
@@ -15,6 +25,13 @@ function getInput(level) {
 }
 
 function expCounter(location) {
-    area = document.getElementById(location);
-    console.log(location);
+    if (location == 'inputArea') {
+        inputArea += 1;
+    } else if (location == 'inputList') {
+        inputList += 1;
+    } else if (location == 'inputItem') {
+        inputItem += 1;
+    } else if (location == 'inputThing') {
+        inputThing += 1;
+    }
 }
